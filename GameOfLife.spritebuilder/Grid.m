f61564e73fac5383f977d03f0445a82b4f219283
John <jzofrea@yahoo.com>
2014-06-30 12:23:28 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -157,11 +157,12 @@ static const int GRID_COLUMNS = 10;
 
     int numAlive = 0;
     Creature *_creature;
+    
     for (int i = 0; i < [_gridArray count]; i ++) {
         for (int j = 0; j < i; j ++) {
             if (j == 3) {
                 _creature.isAlive = YES;
-                numAlive ++; 
+                numAlive ++;
             } else {
                 if ([_gridArray count] <= 1 || [_gridArray count] >= 4) {
                     _creature.isAlive = NO;
